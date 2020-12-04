aea55550e5bdabf6735dc11e433991f615d57755
DILEEP-YADAV <dileep66yadav@gmail.com>
2020-12-04 20:04:11 +0530

    Add files via upload

---
+++ src/main/java/com/saksoft/KafkaVideoSteamingApplication.java
@@ -0,0 +1,13 @@
+package com.saksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaVideoSteamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaVideoSteamingApplication.class, args);
+	}
+
+}
